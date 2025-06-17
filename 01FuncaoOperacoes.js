7bd766f531643a19aff22b6902c7c0d470a7cadd
Andreza Moreira <95940279+AndrezaMor3ira@users.noreply.github.com>
2025-06-16 22:31:14 -0300

    Entrega Atividade1
    Atividade 1, funcao de operações matemáticas

---
+++ 01FuncaoOperacoes.js
@@ -0,0 +1,11 @@
+function Operacoes (num1, num2){
+    soma = num1 + num2
+    subtracao = num1 - num2
+    multiplicacao = num1 * num2
+    divisao = num1/num2
+
+    console.log(`Soma: ${soma}`)
+    console.log(`Subtração: ${subtracao}`)
+    console.log(`Multiplicação: ${multiplicacao}`)
+    console.log(`Divisão: ${divisao}`)
+}
