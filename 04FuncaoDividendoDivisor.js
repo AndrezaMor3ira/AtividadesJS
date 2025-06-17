3d6233a45f9e38de7699922b94972db5faad60b0
Andreza Moreira <95940279+AndrezaMor3ira@users.noreply.github.com>
2025-06-17 11:44:46 -0300

    Entrega Atividade 4
    Função que mostra o resultado e o resto de uma divisão

---
+++ 04FuncaoDividendoDivisor.js
@@ -0,0 +1,7 @@
+function ResultadoDividendoDivisor(dividendo, divisor){
+    resultado = dividendo/divisor
+    resto = dividendo%divisor
+
+    console.log(`O resultado da divisão é: ${resultado}`)
+    console.log(`O resto da divisão é: ${resto}`)
+}
